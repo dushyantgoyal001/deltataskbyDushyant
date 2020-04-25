92e7f5d450538d6c0e90b8bda57352a7beed3233
dushyantgoyal001 <63360305+dushyantgoyal001@users.noreply.github.com>
2020-04-25 21:43:54 +0530

    Add files via upload

---
+++ task.js
@@ -0,0 +1,156 @@
+
+class Stopwatch {
+    constructor(display, results) {
+        this.running = false;
+        this.display = display;
+        this.results = results;
+        this.laps = [];
+        this.reset();
+        this.print(this.times);
+    }
+    
+    reset() {
+        this.times = [ 0, 0, 0 ];
+    }
+    
+    start() {
+        if (!this.time) this.time = performance.now();
+        if (!this.running) {
+            this.running = true;
+            requestAnimationFrame(this.step.bind(this));
+        }
+    }
+    
+    lap() {
+        let times = this.times;
+        let li = document.createElement('li');
+        li.innerText = this.format(times);
+        this.results.appendChild(li);
+    }
+    
+    stop() {
+        this.running = false;
+        this.time = null;
+    }
+
+    restart() {
+        if (!this.time) this.time = performance.now();
+        if (!this.running) {
+            this.running = true;
+            requestAnimationFrame(this.step.bind(this));
+        }
+        this.reset();
+    }
+    
+    clear() {
+        clearChildren(this.results);
+    }
+    
+    step(timestamp) {
+        if (!this.running) return;
+        this.calculate(timestamp);
+        this.time = timestamp;
+        this.print();
+        requestAnimationFrame(this.step.bind(this));
+    }
+    
+    calculate(timestamp) {
+        var diff = timestamp - this.time;
+        // Hundredths of a second are 100 ms
+        this.times[2] += diff / 10;
+        // Seconds are 100 hundredths of a second
+        if (this.times[2] >= 100) {
+            this.times[1] += 1;
+            this.times[2] -= 100;
+        }
+        // Minutes are 60 seconds
+        if (this.times[1] >= 60) {
+            this.times[0] += 1;
+            this.times[1] -= 60;
+        }
+    }
+    
+    print() {
+        this.display.innerText = this.format(this.times);
+    }
+    
+    format(times) {
+        return `\
+${pad0(times[0], 2)}:\
+${pad0(times[1], 2)}:\
+${pad0(Math.floor(times[2]), 2)}`;
+    }
+}
+
+function pad0(value, count) {
+    var result = value.toString();
+    for (; result.length < count; --count)
+        result = '0' + result;
+    return result;
+}
+
+function clearChildren(node) {
+    while (node.lastChild)
+        node.removeChild(node.lastChild);
+}
+
+let stopwatch = new Stopwatch(
+    document.querySelector('.stopwatch'),
+    document.querySelector('.results'));
+	
+	
+	
+	// Get the elements with class="column"
+var elements = document.getElementsByClassName("column");
+
+// Declare a loop variable
+var i;
+
+// List View
+function listView() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.width = "100%";
+  }
+}
+
+// Grid View
+function gridView() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.width = "50%";
+  }
+}
+
+
+
+
+	function myFunction() {
+   document.getElementById("one").style.visibility = "hidden";
+}
+
+	function myFunction2() {
+   document.getElementById("2").style.visibility = "hidden";
+}
+function myFunction3() {
+   document.getElementById("3").style.visibility = "hidden";
+}function myFunction4() {
+   document.getElementById("4").style.visibility = "hidden";
+}function myFunction5() {
+   document.getElementById("5").style.visibility = "hidden";
+}function myFunction6() {
+   document.getElementById("6").style.visibility = "hidden";
+}function myFunction7() {
+   document.getElementById("7").style.visibility = "hidden";
+}function myFunction8() {
+   document.getElementById("8").style.visibility = "hidden";
+}function myFunction9() {
+   document.getElementById("9").style.visibility = "hidden";
+}
+function savefunction(){
+	
+	localStorage.setItem("result", "score");}
+function displayfunction()
+{
+	document.getElementById("score").innerHTML = localStorage.getItem("result");
+	document.getElementById("score").display.alert=localStorage.getItem("score");
+}	
+	//button.style.visibility = "hidden";
